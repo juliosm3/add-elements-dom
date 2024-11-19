26d604b40a88c7e9cc21da2630c619b209409ccc
juliosm3 <tuemailexample.com>
2024-11-19 18:39:41 +0100

    añadir archivos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+agregar.addEventListener('click', () {
+    const newItem = prompt('Ingrese un nuevo elemento para la lista');
+    if (newItem) {
+        const listItem = document.createElement('li');
+        listItem.textContent = newItem;
+        lista.appendChild(listItem);
+    } else {
+        alert('Por favor, ingrese un valor válido.');
+    }
+})
